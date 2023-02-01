199eebb22f0e06f7c88906720d2cd5874a47cb8d
Simona <simona_s@outlook.com>
2023-01-31 21:28:04 -0800

    fixed the comment need in the assignment

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
 if (operator === 'multiplication') {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+/* Do addition
+while (i < 11) {
+  msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+  i++;
+}
+} else {
+*/ 
+//Do multiplication
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
+}
 }
 
 // Write the message into the page
